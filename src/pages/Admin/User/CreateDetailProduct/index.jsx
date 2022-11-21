1a0188cfbc2f3a1f2c14d584ad60ae93dd4d2427
boomkick <tranmanhthang107@gmail.com>
2022-11-22 01:36:02 +0700

    [feature] add_product_50

--- src/pages/Admin/User/CreateDetailProduct/index.jsx
+++ src/pages/Admin/User/CreateDetailProduct/index.jsx
@@ -20,20 +20,18 @@ import { styled } from '@mui/material/styles';
 import rev from "../../../../assets/img/product_le_han_quoc.jpg";
 import SelectBoxAddress from "../../../../components/SelectBoxAddress";
 import { useParams,useNavigate } from "react-router-dom";
-import { productUnit } from "../../../../constraints/Product";
 function CreateDetailProduct(props) {
   const [review, setReview] = React.useState(rev)
   const [edit, setEdit] = useState(props.edit)
-  const [product, setProduct] = useState({})
+  const [brand, setBrand] = useState([])
+  const [country, setCountry] = useState("")
+  const [province, setProvince] = useState("")
+  const [district, setDistrict] = useState("")
+  const [commune, setCommune] = useState("")
   const [name, setName] = useState("")
-  const [category, setCategory] = useState("")
-  const [listCategory, setListCategory] = useState([])
-  const [price, setPrice] = useState("")
-  const [quantity, setQuantity] = useState("")
-  const [unit, setUnit] = useState("")
-  const [minPurchase, setMinPurchase] = useState("")
   const [description, setDescription] = useState("")
-  const [status, setStatus] = useState("")
+  const [phone, setPhone] = useState("")
+  const [addressDetails, setAddressDetails] = useState("")
   const navigate = useNavigate();
   
   const idBrand=useParams().id
@@ -49,142 +47,119 @@ function CreateDetailProduct(props) {
   //   getData();
   // }, []);
 
-  useEffect(() => {
-    const getData = async () => {
-      const params = { id: props.district }
-      setListCategory([]);
-      apiCategory.getAll()
-        .then(res => {
-          setListCategory(res.data.categories);
-        })
-    };
-    getData();
-  }, [listCategory])
-
-  // Change value of select box
-
   const onChangeImg = (e) => {
     if (e.target.files.length > 0) {
       setReview(URL.createObjectURL(e.target.files[0]))
     }
   }
 
-  // handle Add product
+  const handleChangeProvince = (value) => {
 
+    setProvince(value);
+  };
+
+  const handleChangeDistrict = (value) => {
+
+    setDistrict(value);
+  };
+
+
+  const handleChangeCommune = (value) => {
+    setCommune(value);
+  };
   const handleInsert = () => {
     const params = {
-      "Name": name,
-      "CategoryId": category,
-      "Quantity": quantity,
-      "Price": price,
-      "Unit": unit,
-      "MinPurchase": minPurchase,
-      "Description": description,
-      "Status": status,
+      "addressDetails": addressDetails,
+      "country": country,
+      "commune": commune,
+      "description": description,
+      "district": district,
+      "name": name,
+      "phone": phone,
+      "province": province
+
     }
-    if(!(name && category && quantity && price && unit && minPurchase && description && status)) {
+    if(!(addressDetails&& country && commune && description && district && name && phone && province)) {
       toast.warning("Vui lòng nhập đầy đủ thông tin !!");
       return
     }
     else{
-    apiProduct.insertProduct(params)
+    apiBrand.insertBrand(params)
       .then(res => {
-        toast.success("Thêm sản phẩm thành công")
-        setName("")
-        setCategory("")
-        setQuantity("")
-        setPrice("")
-        setUnit("")
-        setMinPurchase("")
+        toast.success("Thêm thương hiệu thành công")
+        setCountry("")
         setDescription("")
-        setStatus("")
+        setPhone("")
+        setName("")
+        setAddressDetails("")
+        setCommune("")
+        setDistrict("")
+        setProvince("")
       })
       .catch(error => {
-        toast.error("Thêm sản phẩm thất bại!")
+        toast.error("Thêm thương hiệu thất bại!")
       })
     }
   }
-
-  // handle update product
-
   const handleUpdate = () => {
     const params = {
-      "Name": name,
-      "CategoryId": category,
-      "Quantity": quantity,
-      "Price": price,
-      "Unit": unit,
-      "MinPurchase": minPurchase,
-      "Description": description,
-      "Status": status,
+      "addressDetails": addressDetails,
+      "country": country,
+      "commune": commune,
+      "description": description,
+      "district": district,
+      "name": name,
+      "phone": phone,
+      "province": province
+
     }
-    if(!(name && category && quantity && price && unit && minPurchase && description && status)) {
+    if(!(addressDetails&& country && commune && description && district && name && phone && province)) {
       toast.warning("Vui lòng nhập đầy đủ thông tin !!");
       return
     }
     else{
-    apiProduct.insertProduct(params,idProduct)
+    apiBrand.updateBrand(params,idBrand)
       .then(res => {
-        toast.success("Sửa sản phẩm thành công")
+        toast.success("Sửa thương hiệu thành công")
       })
       .catch(error => {
-        toast.error("Sửa sản phẩm thất bại!")
+        toast.error("Sửa thương hiệu thất bại!")
       })
     }
   }
-
-  // get data for a particular product
-
-
-  // Set thông tin cho product detail
-  // useEffect(() => {
-  //   const loaddata = () => {
-  //     if (edit === true) {
-  //       apiBrand.getBrandByID({id:idBrand})
-  //         .then(res => {
-  //           const brand = res.data.brand
-  //           console.log(brand)
-  //             if (brand) {
-  //               setName(brand.name)
+  useEffect(() => {
+    const loaddata = () => {
+      if (edit === true) {
+        apiBrand.getBrandByID({id:idBrand})
+          .then(res => {
+            const brand = res.data.brand
+            console.log(brand)
+              if (brand) {
+                setName(brand.name)
                 
-  //               setPhone(brand.phone)
-  //               setAddressDetails(brand.addressDetails)
-  //               setDescription(brand.description)
-  //               setCommune(brand.brandCommune)
-  //               setDistrict(brand.brandDistrict.id)
-  //               setProvince(brand.brandProvince.id)
-  //               setCountry(brand.brandCountry.id)
-  //             }
-  //             else {
-  //               navigate("/admin/brand/create")
-  //               toast.error("Địa chỉ này không tồn tại!")
-  //             }
-  //         })
-  //     }
-  //   }
-  //   loaddata()
-  // }, [edit])
+                setPhone(brand.phone)
+                setAddressDetails(brand.addressDetails)
+                setDescription(brand.description)
+                setCommune(brand.brandCommune.id)
+                setDistrict(brand.brandDistrict.id)
+                setProvince(brand.brandProvince.id)
+                setCountry(brand.brandCountry.id)
+              }
+              else {
+                navigate("/admin/brand/create")
+                toast.error("Địa chỉ này không tồn tại!")
+              }
+          })
+      }
+    }
+    loaddata()
+  }, [edit])
 
   return (
     <Box width={'100%'} bgcolor='#fff'>
       <Stack className="cruBrand" p={3} justifyContent="center" width="700px" spacing={2} bgcolor='#fff'>
         <Stack direction="row">
-          <Typography className="cruBrand__label">Nhập tên sản phẩm</Typography>
-          <TextField value={name} onChange={(event) => { setName(event.target.value) }}
-              size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
-        </Stack>
-        <Stack direction="row">
-          <Typography className="cruBrand__label">Giá</Typography>
-          <TextField value={name} onChange={(event) => { setName(event.target.value) }}
-              size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
-        </Stack>
-        <Stack direction="row">
-          <Typography className="cruBrand__label">Loại sản phẩm</Typography>
-          <TextField value={name} onChange={(event) => { setName(event.target.value) }}
-              size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
-        </Stack>
-        <Stack direction="row">
-          <Typography className="cruBrand__label">Số lượng</Typography>
+          <Typography className="cruBrand__label">Nhập tên thương hiệu</Typography>
           <TextField value={name} onChange={(event) => { setName(event.target.value) }}
               size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
         </Stack>
@@ -193,35 +168,33 @@ function CreateDetailProduct(props) {
           <Typography className="cruBrand__label">Mô Tả</Typography>
           <TextField value={description} onChange={(event) => { setDescription(event.target.value) }} size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
         </Stack>
-
         <Stack direction="row" >
-          <Typography className="cruBrand__label">Tình trạng</Typography>
-          <TextField value={description} onChange={(event) => { setDescription(event.target.value) }} size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
+          <Typography className="cruBrand__label">Địa chỉ</Typography>
+          <TextField value={addressDetails} onChange={(event) => { setAddressDetails(event.target.value) }} size="small" id="outlined-basic" variant="outlined" sx={{ flex: "1" }} />
         </Stack>
 
         <Stack direction="row">
-          <Typography className="cruBrand__label">
-            Đơn vị:
+          <Typography  className="cruBrand__label">
+            Quốc gia:
           </Typography>
           <FormControl className="create-address__input" sx={{flex:"1"}}>
+            <InputLabel id="demo-simple-select-helper-label"></InputLabel>
             <Select
-              size="small"
+              sx={{ flex: 0.65 }}
               labelId="demo-simple-select-helper-label"
               id="demo-simple-select-helper"
-              value={unit}
+              value={country}
               label="Age"
-              onChange={e=>setUnit(e.target.value)}
-              
-              input={<InputCustom placeholder="Chọn đơn vị" />}
+              onChange={e=>setCountry(e.target.value)}
+              input={<InputCustom placeholder="Chọn Quận/Huyện" />}
             >
-              {
-                productUnit.map(item => <MenuItem value={item.id}>{item.text}</MenuItem>)
-              }
+                 <MenuItem value={"1"}>Việt Nam</MenuItem>
+              
             </Select>
           </FormControl>
         </Stack>
 
-        {/* <Stack direction="row">
+        <Stack direction="row">
           <Typography className="cruBrand__label">Liên hệ</Typography>
           <TextField value={phone} onChange={(event) => { setPhone(event.target.value) }} size="small" id="outlined-multiline-flexible"
             multiline
@@ -232,7 +205,7 @@ function CreateDetailProduct(props) {
           onChangeDistrict={handleChangeDistrict}
           onChangeCommune={handleChangeCommune}
           classLabel="cruBrand__label"
-        /> */}
+        />
         <Stack direction="row" p={2}>
           <Typography className="cruBrand__label">Thêm ảnh</Typography>
           <Stack>

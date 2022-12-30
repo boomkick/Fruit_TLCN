e7eefb1724a43ed27df8c5f569dc8d122e411c7f
boomkick <tranmanhthang107@gmail.com>
2022-12-30 23:36:21 +0700

    [fix] api

--- src/apis/apiAddress.js
+++ src/apis/apiAddress.js
@@ -6,48 +6,9 @@ const apiAddress = {
         const res = await axiosClientWithToken.get('/account/profile')
         return res.data;
     },
-
-    getUserAddress: async (params) => {
-        const res = await axiosClientWithToken.get('/user/address')
-        return res.data;
-    },
-    deleteAddressById: async (params) => {
-        const res = await axiosClientWithToken.delete(`/address/${params.id}`)
-        return res.data;
-    },
-    saveAddress: async (params) => {
-        const res = await axiosClientWithToken.post('/user/address', params)
-        return res.data;
-    },
-
-    updateUserAddressById: async (params, id) => {
-        const res = await axiosClientWithToken.post(`/address/${id}`, params)
-        return res.data;
-    },
-    getAddressById: async (params) => {
-        const res = await axiosClientWithToken.get('/address', { params })
-        return res.data;
-    },
     getVietNamLocations: async (params) => {
         const res = await axiosClientWithToken.get('/Location/VietnamLocation')
         return res.data;
     },
-    getGlobalLocations: async (params) => {
-        const res = await axiosClientWithToken.get('/Location/GlobalLocation')
-        return res.data;
-    },
-    // getCommuneInDistrictById: async (params) => {
-    //     const res = await axiosClient.get(`address/commune/${params.id}/district`)
-    //     return res.data;
-    // },
-    // getDistrictInProvinceById: async (params)=>{
-    //     const res= await axiosClient.get(`address/district/${params.id}/province`)
-    //     return res.data;
-    // },
-    // getAllProvince : async (params)=>{
-    //     const res = await axiosClient.get('address/province')
-    //     return res.data;
-    // },
-
 }
 export default apiAddress;
--- src/apis/apiAuth.js
+++ src/apis/apiAuth.js
@@ -6,34 +6,18 @@ const apiAuth = {
         const myLogin = await axiosClient.post('/account/login', params)
         return myLogin.data;
     },
-
     postRegister: async (params) => {
         const register = await axiosClient.post('/account/user/register', params)
         return register.data
     },
-    
     getUserByToken: async (params) => {
         const myLogin = await axiosClient.get('/auth/social', {params})
         return myLogin.data;
     },
-
     search: async (params) => {
         const mySearch = await axiosClient.post('', params)
         return mySearch.data;
     },
-
-    postCheckPhone: async (params) => {
-        const checkPhone = await axiosClient.post('/auth/verification', params)
-        return checkPhone.data
-    },
-    resetPassword:async (params,token) => {
-        const register = await axiosClient.post(`/auth/resetPassword/?token=${token}`, params)
-        return register.data
-    },
-    forgetPassword:async (params) => {
-        const register = await axiosClient.post(`/account/changePassword`, params)
-        return register.data
-    }
 
 }
 
